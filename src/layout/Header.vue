f73b348e4709e18dd4fd037e2e9d81607bacbd6c
changhee9507 <changhee9507@gmail.com>
2020-06-10 17:29:01 +0900

    0610 05:28pm header&nav

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -1,14 +1,78 @@
 <template>
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">홈</router-link>
-        </li>
-        <li>
-          <router-link to="/list">게시판</router-link>
-        </li>
-      </ul>
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div
+        class="row h-100 align-items-center justify-content-center text-center"
+      >
+        <div class="col-lg-10 align-self-end">
+          <a class="nav-link js-scroll-trigger" href="/">
+            <h1 class="text-uppercase text-white font-weight-bold">
+              Happy House
+            </h1>
+          </a>
+          <hr class="divider my-4" />
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">
+            양지용과 윤창희의 프로젝트입니다
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Navigation-->
+    <nav
+      class="navbar navbar-expand-lg navbar-light fixed-top py-3"
+      id="mainNav"
+    >
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"
+          >관통프로젝트</a
+        >
+        <button
+          class="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto my-2 my-lg-0">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger">거래정보 확인</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger">관심지역 확인</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger">관심지역 설정하기</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="/list">공지사항</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="/qna/list"
+                >QnA 게시판</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="/">MyPage</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger">Logout</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
