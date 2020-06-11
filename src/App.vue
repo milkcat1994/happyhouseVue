fa5dfff4a46d5652ca9fe7d5c177461308f9ccb8
milkcat1994 <milkcat1994@gmail.com>
2020-06-11 17:49:25 +0900

    nav 마지막 수정

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <div id="app">
-    <top-header/>
-    <header />
+    <main-header />
     <router-view></router-view>
     <footer />
   </div>
 </template>
 
 <script>
-import Header from "@/layout/Header.vue";
-import TopHeader from "@/layout/TopHeader.vue";
+import MainHeader from "@/layout/Header.vue";
 import Footer from "@/layout/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    TopHeader,
-    Header,
+    MainHeader,
     Footer
   }
 };
